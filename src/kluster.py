7e4d1645abcf3eb40a770c25ef2382a5fee3dffd
Richard J. Marini <richardjmarini@gmail.com>
2014-05-20 16:35:57 -0400

    added cmd line args for documents, etc..

--- src/kluster.py
+++ src/kluster.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
+from optparse import OptionParser, make_option
 from itertools import izip
 from random import choice, sample
 from glob import glob 
 from json import loads, dumps
 from zlib import compress, decompress
 from base64 import b64encode, b64decode
-from os import path
+from os import path, pardir, curdir
 from cPickle import dumps as serialize
 from operator import itemgetter
+from sys import argv
  
 from vespse import Document, TermDocumentMatrix, Vector
-
-docdir= "../documents"
 
 class DocumentKluster(TermDocumentMatrix):
 
 
-   def __init__(self, docdir, k= 3):
+   def __init__(self, k= 3):
 
       super(DocumentKluster, self).__init__()
 
@@ -31,14 +31,29 @@ class DocumentKluster(TermDocumentMatrix):
  
       self.proximity_matrix= izip(keys, [sorted(self.find(self[document_id]), key= lambda result: result[1][0]) for document_id in keys])
       print list(self.proximity_matrix)
-            
+
+def parse_args(argv):
+
+   optParser= OptionParser()
+
+   [optParser.add_option(opt) for opt in [
+      make_option("-d", "--documents", default= path.join(pardir, "documents", "*.dat"), help= "documents directory")
+   ]]
+
+   optParser.set_usage("%prog --query")
+
+   opts, args= optParser.parse_args()
+
+   return opts
 
 if __name__ == '__main__':
 
-   dk= DocumentKluster(docdir= docdir)
+   opts= parse_args(argv)
+
+   dk= DocumentKluster()
 
    i= 0
-   for filename in glob(path.join(docdir, "*.dat")):
+   for filename in glob(opts.documents):
 
       fh= open(filename, "r")
       data= decompress(b64decode(loads(fh.read()).get("payload")))
